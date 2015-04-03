0b582c791bd86c9fa9230a141d2ce8d4d09dd393
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2015-04-03 21:23:25 +0200

    all html content within react app

--- views/main.jsx
+++ views/main.jsx
@@ -5,9 +5,15 @@ var React = require('react');
 var SyntaxChecker = require("./SyntaxChecker.jsx");
 
 module.exports = React.createClass({
+  getDefaultProps: function() {
+    return {
+      initHash: "#syntaxChecker"
+    }
+  },
+
   getInitialState: function () {
     return {
-      hash: (window.location.hash) ? window.location.hash : "#syntaxChecker"
+      hash: (window.location.hash) ? window.location.hash : this.props.initHash
     };
   },
 
@@ -29,25 +35,47 @@ module.exports = React.createClass({
     };
 
     return (
-      <div className="main">
-        <div className="row">
-
-          <div className="col-md-3">
-            <ul className="nav nav-pills nav-stacked">
-              {e("#syntaxChecker", function (id) {
-                return <li className={a(id)}><a href={id} onClick={c} data-toggle="pill">Validate OA / JSON-LD syntax</a></li>;})}
-              {e("#TODO", function (id) {
-                return <li className={a(id)}><a href={id} onClick={c} data-toggle="pill">TODO</a></li>;})}
-            </ul>
+      <div>
+        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div className="container">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#" onClick={function () { self.setState({hash: self.props.initHash}); }}>ACID-RESTful-OA</a>
+            </div>
+            <div id="navbar" className="navbar-collapse collapse">
+            </div>
           </div>
+        </nav>
 
-          <div className="tab-content col-md-9">
-            {e("#syntaxChecker", function (id) {
-              return <div id="syntaxChecker" className={"tab-pane "+a(id)}><SyntaxChecker/></div>;})}
-            {e("#TODO", function (id) {
-              return <div id="TODO" className={"tab-pane"+a(id)}>TODO</div>;})}
+        <div className="container">
+          <div className="main">
+            <div className="row">
+
+              <div className="col-md-3">
+                <ul className="nav nav-pills nav-stacked">
+                  {e("#syntaxChecker", function (id) {
+                    return <li className={a(id)}><a href={id} onClick={c} data-toggle="pill">Validate OA / JSON-LD syntax</a></li>;})}
+                  {e("#TODO", function (id) {
+                    return <li className={a(id)}><a href={id} onClick={c} data-toggle="pill">TODO</a></li>;})}
+                </ul>
+              </div>
+
+              <div className="tab-content col-md-9">
+                {e("#syntaxChecker", function (id) {
+                  return <div id="syntaxChecker" className={"tab-pane "+a(id)}><SyntaxChecker/></div>;})}
+                {e("#TODO", function (id) {
+                  return <div id="TODO" className={"tab-pane"+a(id)}>TODO</div>;})}
+              </div>
+            </div>
           </div>
-      </div>
+        </div>
+        <footer>
+        </footer>
       </div>
     );
   }
