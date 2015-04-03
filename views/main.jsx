6517088a25eb9297f0572d29277f1a5c6cac3aa0
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2015-04-03 21:28:25 +0200

    minor

--- views/main.jsx
+++ views/main.jsx
@@ -5,10 +5,10 @@ var React = require('react');
 var SyntaxChecker = require("./SyntaxChecker.jsx");
 
 module.exports = React.createClass({
-  getDefaultProps: function() {
+  getDefaultProps: function () {
     return {
       initHash: "#syntaxChecker"
-    }
+    };
   },
 
   getInitialState: function () {
@@ -56,16 +56,16 @@ module.exports = React.createClass({
           <div className="main">
             <div className="row">
 
-              <div className="col-md-3">
+              <div className="col-xs-3">
                 <ul className="nav nav-pills nav-stacked">
                   {e("#syntaxChecker", function (id) {
-                    return <li className={a(id)}><a href={id} onClick={c} data-toggle="pill">Validate OA / JSON-LD syntax</a></li>;})}
+                    return <li className={a(id)}><a href={id} onClick={c} data-toggle="pill">Validate OA / JSON-LD</a></li>;})}
                   {e("#TODO", function (id) {
                     return <li className={a(id)}><a href={id} onClick={c} data-toggle="pill">TODO</a></li>;})}
                 </ul>
               </div>
 
-              <div className="tab-content col-md-9">
+              <div className="tab-content col-xs-9">
                 {e("#syntaxChecker", function (id) {
                   return <div id="syntaxChecker" className={"tab-pane "+a(id)}><SyntaxChecker/></div>;})}
                 {e("#TODO", function (id) {
