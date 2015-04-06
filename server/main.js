052a5fb89f6a20f1de5decf7c32597a880384b17
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2015-04-06 20:22:48 +0200

    init implementation of testPOST

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 //var assert = require('assert');
 //var _ = require("lodash");
+var request = require("request");
 var slib = require("./lib.js");
 var lib = require("../src/lib.js");
 
@@ -66,5 +67,33 @@ app.get('/get', function (req, res) {
         if (error) { res.status(400).send(error); }
         else { res.send(data); }
       });
+  }
+});
+
+app.post('/testPOST', function (req, res) {
+  var url = req.query.url || req.body.url;
+  var jsonld = req.body.data || req.body;
+  console.log(url, jsonld);
+  if (!url) {
+    res.status(400).send("You must provide the endpoint url");
+  }
+  else if (!jsonld) {
+    res.status(400).send("You must provide the json-ld data");
+  }
+  else {
+    request.post({url: url, headers: { "Content-Type": "application/ld+json" }, data: jsonld}, function (error, ores, body) {
+      if (error || ores.statusCode !== 200 || ores.statusCode !== 201) {
+        console.log(error);
+        res.status(200).send({valid: false, errors: [(ores.statusCode + "; could not connect -- " + error)], missing: []});
+      }
+      else {
+        var validMsg;
+        switch (ores.statusCode) {
+        case 200: { validMsg = "UPDATED"; break; }
+        case 201: { validMsg = "NEW"; break; }
+        }
+        res.status(200).send({valid: validMsg, errors: [ ], missing: []});
+      }
+    });
   }
 });
