26ae6147869198d77228849ba321bc58457d7a1e
Juan Miguel Cejuela <juanmi@jmcejuela.com>
2015-04-06 10:24:39 +0200

    support for REST POST validate request

--- server/main.js
+++ server/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 //var assert = require('assert');
 //var _ = require("lodash");
-var serverLib = require("./lib.js");
+var slib = require("./lib.js");
+var lib = require("../src/lib.js");
 
 //-------------------------------------------------------------------------
 
@@ -16,6 +18,11 @@ var server = app.listen(process.env.PORT || 3000, function () {
   console.log('App listening at http://%s:%s', "localhost", port);
 });
 
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  extended: true
+}));
+
 //-------------------------------------------------------------------------
 
 app.use("/public", express.static('public'));
@@ -25,10 +32,26 @@ app.get('/', function (req, res) {
 });
 
 app.get('/jsonSchema', function (req, res) {
-  serverLib.getJsonSchema(function (error, data) {
+  slib.getJsonSchema(function (error, data) {
     if (error) { res.status(500).send(error); }
     else { res.send(data); }
   });
+});
+
+var JSON_SCHEMA_URI;
+
+slib.getJsonSchema(function (error, data) {
+  if (error) { console.error("Couldn't get the json schema"); }
+  else {
+    lib.addJsonSchema(data.uri, data.schema);
+    JSON_SCHEMA_URI = data.uri;
+  }
+});
+
+app.post('/validate', function (req, res) {
+  var jsonld = req.body;
+  var ret = lib.validateJsonSyntax(jsonld, JSON_SCHEMA_URI);
+  res.send(ret);
 });
 
 app.get('/get', function (req, res) {
@@ -37,7 +60,7 @@ app.get('/get', function (req, res) {
     res.status(400).send("You must provide a url parameter");
   }
   else {
-    serverLib.getJson(
+    slib.getJson(
       url,
       function (error, data) {
         if (error) { res.status(400).send(error); }
